da1bd8d134af4a1bc3c5446c82e2fd2536f9a076
Aditya Kumar <aditya227192@gmail.com>
2025-06-19 17:07:29 +0530

    Starting Server Fixed Up

--- frontend/src/landing_page/signup/Signup.js
+++ frontend/src/landing_page/signup/Signup.js
@@ -1,10 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import { Link, useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import { clientServer } from '../../axiosConfig';
 import 'react-toastify/dist/ReactToastify.css';
 
 function Signup() {
+
+  useEffect(()=>{
+    async function StartingServer(){
+      await clientServer.get('/');
+    }
+  },[]);
+  
   const navigate = useNavigate();
   const [inputValue, setInputValue] = useState({
     email: "",
